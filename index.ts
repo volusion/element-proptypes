ebfd36a4cc7adf8fd0f5a2b774db686847a7aff7
Cameron Jones <cameron.jones@volusion.com>
2020-06-05 18:24:45 -0500

    wip

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
 import ElementPropTypes from './src/propTypes';
 import extractMetadata from './src/extractMeta';
-import { Proptype } from './src/PropType';
-
-// const p = new Proptype(3)
-// console.log(p.name);
 
 export { ElementPropTypes, extractMetadata };
