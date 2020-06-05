367002bfbcccb4edfb2f7e1aee63f6bb7dd34afc
Cameron Jones <cameron.jones@volusion.com>
2020-06-05 18:22:55 -0500

    convert utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
-const capitalize = string => {
+const capitalize = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const fromCamelToSentence = camelCaseString => {
+const fromCamelToSentence = (camelCaseString: string) => {
     return capitalize(camelCaseString.split(/(?=[A-Z])/).join(' '));
 };
 
