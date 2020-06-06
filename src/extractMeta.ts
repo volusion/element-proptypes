42d0e3380484aa551fe4b5e49683542526064658
Cameron Jones <cameron.jones@volusion.com>
2020-06-05 19:50:48 -0500

    naive typings for extractMetadata

--- src/extractMeta.ts
+++ src/extractMeta.ts
@@ -1,10 +1,11 @@
 /* eslint-disable security/detect-object-injection */
 import { fromCamelToSentence } from './utils';
 
-const extractMetadata = (props = {}) => {
-    const extraction = {};
+const extractMetadata = (props: any = {}) => {
+    const extraction: Record<string, any> = {};
     Object.keys(props).forEach(key => {
-        if (!props[key]) {
+        const prop: any = props[key];
+        if (!prop) {
             return;
         }
         const propType = props[key].type ? props[key].type : props[key];
