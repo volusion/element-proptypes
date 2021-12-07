2fde5315f147dfc974e2fc8ac669af715ac639e7
Nils <nils.devine@volusion.com>
2021-12-07 15:52:55 -0500

    added newest compatible version of jest typeahead plugin

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
     coverageReporters: ["text"],
     rootDir: "src",
     testEnvironment: "node",
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+    ],
     verbose: true,
 };
